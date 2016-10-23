6315740258cbae8aa1a662a98f939dd24a790ffe
Kevin King <4kevinking@gmail.com>
2016-10-23 15:44:23 -0400

    Revert "testing upgrade"
    This reverts commit b32aaa63db6a8545e2dfe14a23c3ae35f1d7b7a5.

--- kr.rb
+++ kr.rb
@@ -2,7 +2,7 @@ require "language/go"
 class Kr < Formula
   desc "Kryptonite command line client, daemon, and SSH integration"
   homepage "https://krypt.co"
-  version "1.0.7"
+  version "1.0.6"
   head "https://github.com/agrinman/kr.git"
   url "-"
 
