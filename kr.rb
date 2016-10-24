50cfd5d895b21eb41c7deb1eada81f16d30ea1cc
Kevin King <4kevinking@gmail.com>
2016-10-24 14:05:16 -0400

    remove devel

--- kr.rb
+++ kr.rb
@@ -16,10 +16,6 @@ class Kr < Formula
 	sha256 "8cc6a9577a7762e09ad5ea06993ab12e8c1effd80266eacaa2e2e0e4c67ae21a" => :yosemite
   end
 
-  devel do
-	  url "https://github.com/agrinman/kr.git", :using => :git
-  end
-
   head do
 	  url "https://github.com/agrinman/kr.git"
   end
