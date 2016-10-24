d37d435f110fff90dd26060d130bf1872eadb2f5
Kevin King <4kevinking@gmail.com>
2016-10-24 13:17:19 -0400

    remove version

--- kr.rb
+++ kr.rb
@@ -2,7 +2,6 @@ require "language/go"
 class Kr < Formula
   desc "Kryptonite command line client, daemon, and SSH integration"
   homepage "https://krypt.co"
-  version "1.0.7"
   url "-"
 
   bottle do
