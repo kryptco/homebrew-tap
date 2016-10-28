b4d49f3a9ac9e7fddd2c92044ed5bb719d33ab00
Kevin King <4kevinking@gmail.com>
2016-10-28 13:05:22 -0400

    remove devel for now

--- kr.rb
+++ kr.rb
@@ -3,9 +3,9 @@ class Kr < Formula
   homepage "https://krypt.co"
   url "https://github.com/kryptco/kr.git", :tag => "1.0.8"
 
-  devel do
-	  url "https://github.com/agrinman/kr.git", :tag => "1.0.9"
-  end
+  #devel do
+	  #url "https://github.com/agrinman/kr.git", :tag => "1.0.9"
+  #end
 
   bottle do
 	root_url "https://github.com/kryptco/bottles/raw/master"
