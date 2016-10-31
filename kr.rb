9f272054098cc2b636010dbf10bc1eaece565856
Kevin King <4kevinking@gmail.com>
2016-10-31 13:47:05 -0400

    update el_capitan

--- kr.rb
+++ kr.rb
@@ -3,9 +3,9 @@ class Kr < Formula
   homepage "https://krypt.co"
   url "https://github.com/kryptco/kr.git", :tag => "1.0.10"
 
-  #devel do
-	  #url "https://github.com/agrinman/kr.git", :tag => "1.0.11"
-  #end
+  devel do
+	  url "https://github.com/agrinman/kr.git", :tag => "1.0.11"
+  end
 
   bottle do
 	root_url "https://github.com/kryptco/bottles/raw/master"
