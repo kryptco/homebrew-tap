7190eb02dcc56b23a6e1a5c13885e04aabc0f7b4
Kevin King <4kevinking@gmail.com>
2016-11-14 14:50:44 -0500

    1.0.13

--- kr.rb
+++ kr.rb
@@ -1,18 +1,18 @@
 class Kr < Formula
   desc "Kryptonite command line client, daemon, and SSH integration"
   homepage "https://krypt.co"
-  url "https://github.com/kryptco/kr.git", :tag => "1.0.12"
+  url "https://github.com/kryptco/kr.git", :tag => "1.0.13"
 
   #devel do
-	  #url "https://github.com/agrinman/kr.git", :tag => "1.0.12"
+	  #url "https://github.com/agrinman/kr.git", :tag => "1.0.13"
   #end
 
   bottle do
 	root_url "https://github.com/kryptco/bottles/raw/master"
 	cellar :any
-	sha256 "03f4991c79a7056bf630cb8ba75e2cd9cc94f1d1500c1eaad01b7998de899ffc" => :yosemite
-	sha256 "683f7790f0d9afc3710cd325c7ad06f1d87f615f5e977cb5c5eddd87b738b54c" => :el_capitan
-	sha256 "dd505dff015eb6ca173ab53f4dd4054876702e2df174c9e5c4b89b0bc08b948f" => :sierra
+	sha256 "c416121d62caf17cf37088a7b101e24d8ff23aaf4706e3de76712ded32701be8" => :yosemite
+	sha256 "2c5bc3287957c9dc5ddfed6a8b58b4bba73145100a2ef7ad6a0d4b7ffccb4046" => :el_capitan
+	sha256 "b863c3574da28f7f3b73f05bb3776b594728c262529f96a383d26cb9bcdad5a7" => :sierra
   end
 
   depends_on "go" => :build
