99cc8ca1b59d6532588bafa8eb2277d276dd6960
Kevin King <4kevinking@gmail.com>
2017-01-23 01:11:37 -0500

    add agent to sshconfig

--- kr.rb
+++ kr.rb
@@ -3,9 +3,9 @@ class Kr < Formula
   homepage "https://krypt.co"
   url "https://github.com/kryptco/kr.git", :tag => "1.0.14"
 
-  #devel do
-	  #url "https://github.com/kryptco/kr.git", :tag => "1.0.14"
-  #end
+  devel do
+	  url "https://github.com/kryptco/kr.git", :tag => "1.0.15"
+  end
 
   bottle do
 	root_url "https://github.com/kryptco/bottles/raw/master"
@@ -45,7 +45,7 @@ class Kr < Formula
 
   def post_install
 	  #	add PKCS11Provider to ssh_config if not present
-	  system "touch ~/.ssh/config; perl -0777 -ne '/\\n# Added by Kryptonite\\nHost \\*\\n\\tPKCS11Provider \\/usr\\/local\\/lib\\/kr-pkcs11.so/ || exit(1)' ~/.ssh/config || echo \"\\\\n# Added by Kryptonite\\nHost *\\\\n\\\\tPKCS11Provider /usr/local/lib/kr-pkcs11.so\" >> ~/.ssh/config"
+	  system "touch ~/.ssh/config; perl -0777 -ne '/\\n# Added by Kryptonite\\nHost \\*\\n\\tIdentityAgent ~\\/.kr\\/krd-agent.sock\\n\\tPKCS11Provider \\/usr\\/local\\/lib\\/kr-pkcs11.so/ || exit(1)' ~/.ssh/config || echo \"\\\\n# Added by Kryptonite\\nHost *\\\\n\\\\tIdentityAgent ~\\\\/.kr\\\\/krd-agent.sock\\\\n\\\\n\\\\tPKCS11Provider /usr/local/lib/kr-pkcs11.so\" >> ~/.ssh/config"
 	  system "mkdir -p ~/Library/LaunchAgents; cp /usr/local/share/kr/co.krypt.krd.plist ~/Library/LaunchAgents"
 	  system "launchctl unload ~/Library/LaunchAgents/co.krypt.krd.plist || true"
 	  system "launchctl load ~/Library/LaunchAgents/co.krypt.krd.plist"
