fcbe4aca9d9d6786c1e5f6fe011af6a9694b5ebd
Kevin King <kevin@krypt.co>
2017-08-08 17:27:28 -0400

    test no post_install

--- kr.rb
+++ kr.rb
@@ -58,23 +58,10 @@ class Kr < Formula
   end
   
   def post_install
-	  escaped_prefix = HOMEBREW_PREFIX.to_s.gsub '/', '\\/'
-	  if build.with? "no-ssh-config"
-	  else
-		  system "mkdir -p ~/.ssh"
-		  # remove old ssh_config entries
-		  system "touch ~/.ssh/config"
-		  system "perl -0777 -p -i.bak1 -e 's/\s*#Added by Kryptonite\\nHost \\*\\n\\tPKCS11Provider \\/usr\\/local\\/lib\\/kr-pkcs11.so\\n\\tProxyCommand `find \\/usr\\/local\\/bin\\/krssh 2>\\/dev\\/null \\|\\| which nc` %h %p\\n\\tIdentityFile ~\\/.ssh\\/id_kryptonite\\n\\tIdentityFile ~\\/.ssh\\/id_ed25519\\n\\tIdentityFile ~\\/.ssh\\/id_rsa\\n\\tIdentityFile ~\\/.ssh\\/id_ecdsa\\n\\tIdentityFile ~\\/.ssh\\/id_dsa//g' ~/.ssh/config" 
-
-		  # add current ssh_config
-		  system "perl -0777 -ne '/# Added by Kryptonite\\nHost \\*\\n\\tPKCS11Provider #{escaped_prefix}\\/lib\\/kr-pkcs11.so\\n\\tProxyCommand #{escaped_prefix}\\/bin\\/krssh %h %p\\n\\tIdentityFile ~\\/.ssh\\/id_kryptonite\\n\\tIdentityFile ~\\/.ssh\\/id_ed25519\\n\\tIdentityFile ~\\/.ssh\\/id_rsa\\n\\tIdentityFile ~\\/.ssh\\/id_ecdsa\\n\\tIdentityFile ~\\/.ssh\\/id_dsa/ || exit(1)' ~/.ssh/config || echo \"\\\\n# Added by Kryptonite\\nHost *\\\\n\\\\tPKCS11Provider #{HOMEBREW_PREFIX}/lib/kr-pkcs11.so\\\\n\\\\tProxyCommand #{HOMEBREW_PREFIX}/bin/krssh %h %p\\\\n\\\\tIdentityFile ~/.ssh/id_kryptonite\\\\n\\\\tIdentityFile ~/.ssh/id_ed25519\\\\n\\\\tIdentityFile ~/.ssh/id_rsa\\\\n\\\\tIdentityFile ~/.ssh/id_ecdsa\\\\n\\\\tIdentityFile ~/.ssh/id_dsa\" >> ~/.ssh/config"
-	  end
-	  system "mkdir -p ~/Library/LaunchAgents; cat #{prefix}/share/kr/co.krypt.krd.plist | sed -E 's/\\/usr\\/local/#{escaped_prefix}/g' > ~/Library/LaunchAgents/co.krypt.krd.plist"
-	  system "kr restart"
   end
 
    def caveats
-	   return "Installation with brew is temporarily disabled, please install kr with `curl https://krypt.co/kr | sh`"
+	   return "kr is now installed! Run `kr pair` to pair with the Kryptonite app."
    end
 
 end
